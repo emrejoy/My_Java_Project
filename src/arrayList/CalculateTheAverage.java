df1a00977614e83fe828c638711dc8d34479ddd0
Emre Durmaz <emredrmz@gmail.com>
2023-04-28 22:54:17 -0400

    calculate the average value of array elements.

---
+++ src/arrayList/CalculateTheAverage.java
@@ -0,0 +1,25 @@
+package arrayList;
+
+public class CalculateTheAverage {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[]{20, 30, 40, 50, 60};
+        //calculate sum of all array elements,
+        int sum = 0;
+        double average = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+
+            average = sum / numbers.length;
+
+
+        }
+        System.out.println("Average value of the array elements is : " + average);
+    }
+
+}
+
+
+//Write a Java program to calculate the average value of array elements.
